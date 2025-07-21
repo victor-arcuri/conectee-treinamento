c93b6ca8781bc94ccfe4585a2a24134f76d56bb1
Victor Romano Arcuri <victor.r.arcuri@gmail.com>
2025-07-21 18:31:48 -0300

    Correções no container

--- fts-postgres/init_db.sh
+++ fts-postgres/init_db.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# Cria o banco de dados
-psql -U postgres -d postgres -c "CREATE DATABASE
-BD_PESQUISADOR
+
+psql -U postgres -d postgres -c "
+CREATE DATABASE BD_PESQUISADOR
 WITH
 OWNER = postgres
 ENCODING = 'UTF8'
@@ -9,4 +9,9 @@ LC_COLLATE = 'pt_BR.UTF-8'
 LC_CTYPE = 'pt_BR.UTF-8'
 TABLESPACE = pg_default
 CONNECTION LIMIT = -1
-IS_TEMPLATE = False;"
+IS_TEMPLATE = False;
+"
+
+psql -U postgres -d BD_PESQUISADOR -c "CREATE EXTENSION IF NOT EXISTS \"uuid-ossp\";"
+
+psql -U postgres -d BD_PESQUISADOR -f /docker-entrypoint-initdb.d/tables.sql
--- fts-postgres/sql/simcc/tables.sql
+++ fts-postgres/sql/simcc/tables.sql
@@ -1,20 +1,25 @@
--- Extensão UUID-OSSP
-CREATE EXTENSION "uuid-ossp";
-
--- Cria tabela dos pesquisadores
+-- Criação da tabela pesquisadores
 CREATE TABLE IF NOT EXISTS pesquisadores (
-pesquisadores_id UUID NOT NULL DEFAULT uuid_generate_v4(),
-lattes_id VARCHAR(16) NOT NULL,
-nome VARCHAR(200) NOT NULL,
-PRIMARY KEY (pesquisadores_id ));
+    pesquisadores_id UUID NOT NULL DEFAULT uuid_generate_v4(),
+    lattes_id VARCHAR(16) NOT NULL,
+    nome VARCHAR(200) NOT NULL,
+    PRIMARY KEY (pesquisadores_id)
+);
 
--- Cria tabela de produções
+-- Criação da tabela producoes
 CREATE TABLE IF NOT EXISTS producoes (
-producoes_id UUID NOT NULL DEFAULT uuid_generate_v4(),
-pesquisadores_id UUID NOT NULL,
-issn VARCHAR(16) NOT NULL,
-nomeArtigo TEXT NOT NULL,
-anoArtigo INTEGER NOT NULL,
-PRIMARY KEY (producoes_id),
-CONSTRAINT fkey FOREIGN KEY ( pesquisadores_id)
-REFERENCES pesquisadores (pesquisadores_id) ON UPDATE NO ACTION ON DELETE NO ACTION);
+    producoes_id UUID NOT NULL DEFAULT uuid_generate_v4(),
+    pesquisadores_id UUID NOT NULL,
+    issn VARCHAR(16) NOT NULL,
+    nomeArtigo TEXT NOT NULL,
+    anoArtigo INTEGER NOT NULL,
+    PRIMARY KEY (producoes_id),
+    CONSTRAINT fkey FOREIGN KEY (pesquisadores_id)
+        REFERENCES pesquisadores (pesquisadores_id)
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+);
+
+-- Importação de dados
+\COPY pesquisadores(lattes_id, nome) FROM '/docker-entrypoint-initdb.d/pesquisadores.csv' DELIMITER ',' CSV HEADER;
+\COPY producoes(pesquisadores_id, issn, nomeArtigo, anoArtigo) FROM '/docker-entrypoint-initdb.d/producoes.csv' DELIMITER ',' CSV HEADER;
