395783a495ced3dec5c9c041927f3248525ff9f3
Victor Romano Arcuri <victor.r.arcuri@gmail.com>
2025-07-21 18:47:23 -0300

    Correções no init_db.sh

--- fts-postgres/init_db.sh
+++ fts-postgres/init_db.sh
@@ -7,10 +7,12 @@ OWNER = postgres
 ENCODING = 'UTF8'
 LOCALE_PROVIDER = icu
 ICU_LOCALE = 'pt-BR'
+TEMPLATE = template0
 TABLESPACE = pg_default
 CONNECTION LIMIT = -1
 IS_TEMPLATE = False;
 "
+
 
 psql -U postgres -d BD_PESQUISADOR -c "CREATE EXTENSION IF NOT EXISTS \"uuid-ossp\";"
 
